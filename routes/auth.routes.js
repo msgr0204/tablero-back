a570486e96f9e230d1401567d6097f5a84e84e97
msgr0204 <msgr0204@gmail.com>
2026-06-29 15:08:45 -0500

    Se modifico prueba

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,9 @@ const authController = require('../controllers/auth.controllers');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', authController.register);
+//Recordar rutas de auth con numero celular
 router.post('/login', authController.login);
+
 router.get('/perfil', authMiddleware, authController.getPerfil);
 
 module.exports = router;
